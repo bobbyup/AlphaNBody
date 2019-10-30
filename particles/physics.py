3e5b231eb8cf97710ce076831ede5597d3b2d732
bobbyup <57191881+bobbyup@users.noreply.github.com>
2019-10-30 17:09:46 -0400

    Update physics.py

--- particles/physics.py
+++ particles/physics.py
@@ -9,14 +9,14 @@ Defines update functions for particles
 K = 9.81
 
 
-def zero_update_function(particle, thrust, step_length):
+def zero_update_function(particle, thrust, step_length, thrust_velocity, thrust_degree):
     state = list(particle.state_list[-1])
     state[0] += step_length
     return tuple(state)
 
 
 def gravitaional_pull_from_list(particle_list):
-    def update_function(particle, thrust, step_length):
+    def update_function(particle, thrust, step_length,thrust_velocity, thrust_degree):
         net_f = np.zeros((2, 1))
         time, position, velocity, fuel = particle.state_list[-1]
 
@@ -28,16 +28,19 @@ def gravitaional_pull_from_list(particle_list):
                 net_f += force_p
 
         # Add Thrust Logic Here #
-
+        # I am assuming that thrust is in kilograms
+        # Formula is dv = v_thrust log(mass initial/mass final)
+        dv_thrust = thrust_velocity np.log(particle.state_list[-1][-1]/(particle.state_list[-1][-1] - thrust))
+        dv_thrust = np.array([dv_thrust np.cos(thrust_degree), dv_thrust np.sin(thrust_degree)])
         #########################
         net_a = net_f/particle.mass
-        dv = net_a * step_length
+        dv = dv_thrust + net_a * step_length
         avg_v = velocity + .5 * dv
 
         next_time = time + step_length
         next_position = position + avg_v * step_length
         next_velocity = velocity + dv
-        next_fuel = fuel
+        next_fuel = fuel - thrust
 
         return (next_time, next_position, next_velocity, next_fuel)
     return update_function
